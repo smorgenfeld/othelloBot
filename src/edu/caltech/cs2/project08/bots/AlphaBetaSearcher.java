20f55a6aa2fa7a28930dc2e985cfaad407a987d6
smorgenfeld <smorgenfeld@users.noreply.github.com>
2019-03-08 19:45:34 -0800

    alphaBeta pruning (?) - can't test until Adam fixes the Play import errors

--- src/edu/caltech/cs2/project08/bots/AlphaBetaSearcher.java
+++ src/edu/caltech/cs2/project08/bots/AlphaBetaSearcher.java
@@ -3,8 +3,11 @@ package edu.caltech.cs2.project08.bots;
 import edu.caltech.cs2.project08.game.Board;
 import edu.caltech.cs2.project08.game.Evaluator;
 import edu.caltech.cs2.project08.game.Move;
+import edu.caltech.cs2.project08.interfaces.IDeque;
 
 public class AlphaBetaSearcher<B extends Board> extends AbstractSearcher<B> {
+    private static final float PERCENTAGE_SEQUENTIAL = 0.5f;
+    private static final int INFINITY = 999999999;
     @Override
     public Move getBestMove(B board, int myTime, int opTime) {
         BestMove best = alphaBeta(this.evaluator, board, ply);
@@ -12,6 +15,52 @@ public class AlphaBetaSearcher<B extends Board> extends AbstractSearcher<B> {
     }
 
     private static <B extends Board> BestMove alphaBeta(Evaluator<B> evaluator, B board, int depth) {
-        return null;
+
+        //int cores = Runtime.getRuntime().availableProcessors();
+        IDeque<Move> childs = board.getMoves();
+        Move bestMove = childs.peek();
+        int bestVal = -INFINITY;
+        int curVal;
+        for (Move child : childs) {
+            curVal = prune(evaluator, board, 4, -INFINITY, INFINITY);
+            if (curVal > bestVal) {
+                bestVal = curVal;
+                bestMove = child;
+            }
+        }
+        return new BestMove(bestMove, bestVal);
+    }
+
+    private static <B extends Board> int prune(Evaluator<B> evaluator, B board, int curDepth, int a, int b) {
+        if (curDepth == 0 || board.isGameOver()) {
+            return board.getScore();
+        }
+        int value;
+        if (board.isBlackMove()) {
+            value = -INFINITY;
+            for (Move child : board.getMoves()) {
+                board.makeMove(child);
+                value = Math.max(value, prune(evaluator, board, curDepth - 1, a, b));
+                a = Math.max(a, value);
+                if (a >= b) {
+                    break;
+                }
+                board.undoMove();
+            }
+            return value;
+        }
+        else {
+            value = INFINITY;
+            for (Move child : board.getMoves()) {
+                board.makeMove(child);
+                value = Math.min(value, prune(evaluator, board, curDepth - 1, a, b));
+                b = Math.min(b, value);
+                if (a >= b) {
+                    break;
+                }
+                board.undoMove();
+            }
+            return value;
+        }
     }
 }
